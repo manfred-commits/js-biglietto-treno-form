18ca9da57d4b2ce785103a9f951370c10f77854e
manfred-commits <manfredipiraino@gmail.com>
2021-08-30 17:49:36 +0200

    began to work on the js to connect the input tags

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// 1. First, it's necessary to save the buttons that will drastically affect the page. The necessity of this action, stem from the fact that we will later have to 'toggle' with the use of the buttons the information of the exercise.
+var btnGenerate=document.getElementById("generate").value;
+var btnCancel=document.getElementById("cancel").value;
+
+// 2. for each button we'll have to generate an event using the addEventListener property, to allow us to execute the action of calculating the price by clicking on the designate button
+btnGenerate.addEventListener('click',
+    function(){
+
+    }
+);
